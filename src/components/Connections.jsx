83ad363711e19a49518ba3770fd8fd574e75c424
Olivia Ruiz-Knott <livvy@hovercraftstudio.com>
2024-01-02 19:28:14 -0700

    add connection component

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -5,7 +5,31 @@ const Connections = () => {
   const CONNECTIONS_DATA = useGetValue("CONNECTIONS_DATA", null);
   const GUEST_1 = useGetValue("GUEST_1", null);
 
-  return <div className="Connections quarter">Connections</div>;
+  const renderConnections = useCallback(() => {
+    if (!CONNECTIONS_DATA || !GUEST_1) return;
+    const guestConnections = CONNECTIONS_DATA[GUEST_1].connections;
+
+    return (
+      <div>
+        {Object.entries(guestConnections)
+          .sort((a, b) => b[1].count - a[1].count)
+          .map(([key, value]) => {
+            return (
+              <div key={key}>
+                {value.count} {value.name}
+              </div>
+            );
+          })}
+      </div>
+    );
+  }, [CONNECTIONS_DATA, GUEST_1]);
+
+  return (
+    <div className="Connections quarter">
+      <div>CONNECTIONS</div>
+      {GUEST_1 && renderConnections()}
+    </div>
+  );
 };
 
 export default Connections;
