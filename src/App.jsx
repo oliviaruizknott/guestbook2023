5b8b87fdf9dc059579f621dff3a15d3877ac4476
Olivia Ruiz-Knott <livvy@hovercraftstudio.com>
2024-01-02 22:04:33 -0700

    add moments

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,30 @@ import Calendar from "./components/Calendar";
 import Moments from "./components/Moments";
 
 import guestsData from "./data/guests_data.json";
+import momentsData from "./data/moments_data.json";
 
 const App = () => {
   const appStoreConnected = useGetValue("AppStoreDistributed_CONNECTED", null);
   const NFC_UID = useGetValue("NFC_UID", null);
+  const GUEST_1 = useGetValue("GUEST_1", null);
 
   // at app start, populate the store with our json data
   useEffect(() => {
     if (!appStoreConnected) return;
     setValue("GUESTS_DATA", guestsData);
+    setValue("MOMENTS_DATA", momentsData);
   }, [appStoreConnected]);
+
+  ////////////////////////////////////////////
+  // LISTENERS
+  // set up listeners for various key changes
+  ////////////////////////////////////////////
+
+  // when GUEST_1 changes, update MOMENT_IDS
+  useEffect(() => {
+    if (!GUEST_1) setValue("MOMENT_IDS", null);
+    if (GUEST_1) setValue("MOMENT_IDS", guestsData[GUEST_1].moments);
+  }, [GUEST_1]);
 
   return (
     <div className="App">
--- src/components/Moments.jsx
+++ src/components/Moments.jsx
@@ -2,10 +2,47 @@ import { useCallback } from "react";
 import { useGetValue, setValue } from "../hooks/useHaxademicStore";
 
 const Moments = () => {
-  const GUESTS_DATA = useGetValue("GUESTS_DATA", null);
-  const GUEST_1 = useGetValue("GUEST_1", null);
+  const MOMENTS_DATA = useGetValue("MOMENTS_DATA", null);
+  const MOMENT_IDS = useGetValue("MOMENT_IDS", null);
 
-  return <div className="Moments quarter">Moments</div>;
+  const renderAllMoments = useCallback(() => {
+    return (
+      <div>
+        {Object.entries(MOMENTS_DATA)
+          .sort((a, b) => b[1].date - a[1].date)
+          .map(([key, value]) => {
+            return (
+              <div key={key}>
+                {value.guest_count} {value.title}
+              </div>
+            );
+          })}
+      </div>
+    );
+  }, [MOMENTS_DATA]);
+
+  const renderMoments = useCallback(() => {
+    return (
+      <div>
+        {MOMENT_IDS.map((momentId) => {
+          const momentData = MOMENTS_DATA[momentId];
+          return (
+            <div key={momentId}>
+              {momentData.guest_count} {momentData.title}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }, [MOMENTS_DATA, MOMENT_IDS]);
+
+  return (
+    <div className="Moments quarter">
+      <div>MOMENTS</div>
+      {MOMENTS_DATA && !MOMENT_IDS && renderAllMoments()}
+      {MOMENTS_DATA && MOMENT_IDS && renderMoments()}
+    </div>
+  );
 };
 
 export default Moments;
